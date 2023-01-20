9e16ddac5526e17be8faea690357498e96367a5e
raharimanantenadiary <87566881+raharimanantenadiary@users.noreply.github.com>
2023-01-20 13:24:43 +0300

    partie Client, template login et inscription avec acceuil

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -2,7 +2,10 @@ import { FooterComponent } from './composant/footer/footer.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './authentification/login/login.component';
@@ -11,6 +14,10 @@ import { AcceuilComponent } from './composant/client/acceuil/acceuil.component';
 import { TemplateComponent } from './composant/template/template.component';
 import { HeaderComponent } from './composant/header/header.component';
 import { NavbarComponent } from './composant/navbar/navbar.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { SidebarComponent } from './composant/sidebar/sidebar.component';
+import { HistoriqueComponent } from './composant/client/historique/historique.component';
 
 @NgModule({
   declarations: [
@@ -21,11 +28,18 @@ import { NavbarComponent } from './composant/navbar/navbar.component';
     TemplateComponent,
     HeaderComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    SidebarComponent,
+    HistoriqueComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
+    DragDropModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: LoginComponent },
       { path: 'login', component: LoginComponent },
@@ -35,6 +49,7 @@ import { NavbarComponent } from './composant/navbar/navbar.component';
       { path: 'navbar', component: NavbarComponent },
       { path: 'header', component: HeaderComponent },
       { path: 'footer', component: FooterComponent },
+      { path: 'historique', component: HistoriqueComponent },
     ])
   ],
   providers: [],
--- Server/.history/routes/UserRoute_20230120123438.js
+++ Server/.history/routes/UserRoute_20230120123438.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var cors = require('cors');
 var router = express.Router();
 
+
+router.use(cors());
 
 const { findAll, save } = require('../controller/UserController') ;
 /* GET users listing. */

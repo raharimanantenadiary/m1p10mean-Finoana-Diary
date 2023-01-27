6f06ea6c0369fad5279265799b9da2386dfbe1fd
ETU1340 <noumsfinoana@gmail.com>
2023-01-27 15:19:04 +0300

    recuperation voiture

--- Server/controller/VoitureController.js
+++ Server/controller/VoitureController.js
@@ -1,18 +1,11 @@
 const Voiture = require("../models/Voiture") ;
 const User = require("../models/User") ;
 const mongoose=require("mongoose");
-
-
+const Reparation = require("../models/Reparation") ;
+const Depot = require('../models/Depot') ;
 const findAll = async (req, res) => {
     console.log(req.params);
-//     User.findOne({_id:req.params.idclient},{voiture:1}).populate({path:'voiture.id',match:{etat:0}})
-//     .exec(function (err, facture) {
-//         if (err) {
-//             sendResult(res,err);
-//         } else {
-//         sendResult(res, facture);
-//     }
-// });
+
 
 let idclient=mongoose.Types.ObjectId(req.params.idclient);
 
@@ -89,7 +82,7 @@ const save = async (req, res) => {
                 { $push: { voiture: {id:voiture.id} }}, // add the new role to the 'roles' array
                 { new: true }, // return the updated document
                 (err, user) => {
-                    if (err) return handleError(err);
+                    if (err) return  sendResult(res,err);
                     sendResult(res,user);
                 }
             );
@@ -103,16 +96,93 @@ const save = async (req, res) => {
 
 
 const recuperation = async (req, res) => {
-    Voiture.updateOne ({ _id: req.body.idvoiture}, 
-        { $set: {etat: 0 } }, function(err, result) {
-            if (err) {
-                sendResult(res, err);
-            } else {
-              console.log(result);
+    console.log(req.params);
+    let idvoiture=mongoose.Types.ObjectId(req.params.idvoiture);
+    Reparation.aggregate([
+        {
+          $lookup:
+            {
+              from: "depots",
+              localField: "iddepot",
+              foreignField: "_id",
+              as: "depot"
             }
-          });
+        },
+        {
+          $unwind: "$depot"
+        },
+
+        
+        {
+            $match: {
+                $and: [
+                    {"depot.idvoiture":idvoiture},
+                    {"depot.etat":2}
+                ]
+                
+            }
+        },
+        {
+            $lookup:
+            {
+                from: "bonsorties",
+                localField: "idbonsortie",
+                foreignField: "_id",
+                as: "bonsortie"
+            }
+        },
+        {
+            $unwind: "$bonsortie"
+        },
+    
+        
+        {
+            $project:
+            {
+             depot:1,
+             bonsortie:1
+            }
+        }
        
-}
+      ]).exec(function (err, recup) {
+        if (err) {
+            sendResult(res, err);
+        } else {
+       console.log(recup[0]);
+            if(recup[0].bonsortie.etatLivraison!=1)
+               {
+                return  res.send({message:"bon de sortie non validé",error:true});
+               }
+               else
+               {
+             Voiture.updateOne({ _id: recup[0].depot.idvoiture},{ $set: {etat:0} },(err,voiture) => {
+            if (err) return sendResult(res,err);
+             Depot.updateOne({idvoiture: recup[0].depot.idvoiture,etat:2},{$set:{etat:3}},(err,depot) => {
+                if (err) return sendResult(res,err);
+                res.send({message:"voiture recupere",error:false});
+             }) 
+             
+        });
+       }
+               }
+    })
+    //    if(recup.idbonsortie.etatLivraison!=1)
+    //    {
+    //     return  res.send({message:"bon de sortie non validé",error:true});
+    //    }
+    //    else
+    //    {
+    //     Voiture.updateOne({ _id: recup.iddepot.idvoiture},{ $set: {etat:0} },(err,voiture) => {
+    //         if (err) return sendResult(res,err);
+    //         res.send({message:"bon de sortie non validé",error:false})
+    //     });
+    //    }
+   
+      
+    };
+   
+       
+
 
 
 

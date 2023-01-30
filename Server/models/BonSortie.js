a92d1df51b5520698f1c4fe41e62f80cb38efa86
ETU1340 <noumsfinoana@gmail.com>
2023-01-30 11:44:17 +0300

    reference bs et facture

--- Server/models/BonSortie.js
+++ Server/models/BonSortie.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema ;
 const ObjectId = require("mongodb").ObjectId ;
 
 const BonSchema = new Schema({
-
+    reference:{ type: String,unique: true},
     etatLivraison:{type: String, required: true},
     dateSortie:{type: Date, required: true},
 
@@ -12,5 +12,18 @@ const BonSchema = new Schema({
       
 
 }) ;
+
+BonSchema.pre("save", function (next) {
+    if (!this.reference) {
+      this.reference = generateUniqueRef();
+    }
+    next();
+  });
+  let refCounter = 0;
+  function generateUniqueRef() {
+    refCounter++;
+    const bonsortie ='BS';
+    return `${bonsortie}-${String(refCounter).padStart(4, "0")}`;
+  }
 
 module.exports = mongoose.model("BonSortie",BonSchema) ;
--- Server/models/Facture.js
+++ Server/models/Facture.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose') ;
 const Schema = mongoose.Schema ;
-const ObjectId = require("mongodb").ObjectId ;
+
 
 const FactureSchema = new Schema({
-   
+    reference:{ type: String,unique: true},
     datefacture:{type: Date,default: Date.now, required: true},
     etat:{type: Number, required: true},
    
      
 
-   
-
       
 
 }) ;
+
+FactureSchema.pre("save", function (next) {
+    if (!this.reference) {
+      this.reference = generateUniqueRef();
+    }
+    next();
+  });
+  let refCounter = 0;
+  function generateUniqueRef() {
+    refCounter++;
+    const facture ='FCT';
+    return `${facture}-${String(refCounter).padStart(4, "0")}`;
+  }
+
 
 module.exports = mongoose.model("Facture",FactureSchema) ;

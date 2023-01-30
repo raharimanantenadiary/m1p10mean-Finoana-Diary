3bc1641350b6af8eb939a5ea44246b98482d8a4c
raharimanantenadiary <87566881+raharimanantenadiary@users.noreply.github.com>
2023-01-30 19:00:06 +0300

    rectification

--- Server/app.js
+++ Server/app.js
@@ -54,25 +54,25 @@ app.use('/api/facture', factureRouter);
 app.use('/api/bonsortie', bonsortieRouter);
 app.use('/api/depense',depenseRouter);
 
-const port = process.env.PORT;
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+// const port = process.env.PORT;
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-// module.exports = app;
+module.exports = app;
